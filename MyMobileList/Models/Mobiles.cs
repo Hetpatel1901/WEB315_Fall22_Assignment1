ad95b1f2c6aa0cd8997076093e0b99dde128bf31
Hetpatel1901 <W0805024@myscc.ca>
2022-06-03 18:05:57 -0400

    Mobiles models
    added Mobiles.cs in models folder

---
+++ MyMobileList/Models/Mobiles.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMobiles.Models
+{
+    public class Mobilephones
+    {
+        public int ID { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Year { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Network { get; set; }
+
+        public int Displaysize { get; set; }
+    }
+}
