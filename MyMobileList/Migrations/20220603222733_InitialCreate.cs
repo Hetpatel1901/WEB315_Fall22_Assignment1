af0e79363e77d9a9c176cf99fd5fbe2301a38b51
Hetpatel1901 <W0805024@myscc.ca>
2022-06-03 18:31:23 -0400

    Database added and Startup file edited. README.md file edited.
    Database and initialize migration command ran succesfully and README.md file edited. Did some changes in Startup file.

---
+++ MyMobileList/Migrations/20220603222733_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyMobileList.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Mobilephones",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Brand = table.Column<string>(type: "TEXT", nullable: true),
+                    Model = table.Column<string>(type: "TEXT", nullable: true),
+                    Year = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Network = table.Column<string>(type: "TEXT", nullable: true),
+                    Displaysize = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mobilephones", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mobilephones");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -49,4 +49,7 @@ Command to scafhold the page
 
 ---------------------------------------------------------------------------------------------------
 
+Database and Migration added from this command
+1. dotnet ef migrations add InitialCreate
+2. dotnet ef database update
 
